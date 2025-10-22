6ac9b9ccada46eb0e7d172176e34f411348e4258
attackor7 <x1011000@163.com>
2025-10-22 21:22:39 +0800

    Twitter-CloudMusic 0.0.3-TEST

--- src/config.ts
+++ src/config.ts
@@ -59,7 +59,7 @@ export const profileConfig: ProfileConfig = {
 	links: [
 		{
 			name: "CloudMusic",
-			icon: "ri:netease-cloud-music-fill", // Visit https://icones.js.org/ for icon codes
+			icon: "simple-icons:netease-cloud-music", // Visit https://icones.js.org/ for icon codes
 			// You will need to install the corresponding icon set if it's not already included
 			// `pnpm add @iconify-json/<icon-set-name>`
 			url: "https://music.163.com/#/user/home?id=",
