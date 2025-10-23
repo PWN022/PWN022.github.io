0a123600fcabe2f8aeadbc63a29c95edb515125b
attackor7 <x1011000@163.com>
2025-10-23 21:21:40 +0800

    修改BUG 0.0.3-TEST

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-  darkMode: "class", // allows toggling dark mode manually
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
-}
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
+	darkMode: "class", // allows toggling dark mode manually
+	theme: {
+		extend: {
+			colors: {
+				link: "#0366d6", // 默认链接色
+				"link-hover": "#024ea4", // 悬停加深
+			},
+			fontFamily: {
+				sans: ["Roboto", "sans-serif", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/typography")],
+};
